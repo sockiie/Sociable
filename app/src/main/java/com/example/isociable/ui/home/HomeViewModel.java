e3fe7573d4a977642d5274b72f7ab05139c7281e
sockiie <68150333+sockiie@users.noreply.github.com>
2021-06-30 13:18:52 +0300

    Initial commit homescreen imageview etc

--- app/src/main/java/com/example/isociable/ui/home/HomeViewModel.java
+++ app/src/main/java/com/example/isociable/ui/home/HomeViewModel.java
@@ -10,7 +10,7 @@ public class HomeViewModel extends ViewModel {
 
     public HomeViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+    /*    mText.setValue("@string/hello_nature_lover");*/
     }
 
     public LiveData<String> getText() {
