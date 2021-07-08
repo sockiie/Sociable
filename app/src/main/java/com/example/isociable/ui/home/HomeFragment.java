2f463f268457e48c09d1df006fc1536761d3f001
sockiie <68150333+sockiie@users.noreply.github.com>
2021-07-08 11:38:03 +0200

    Initial commit sehr viel ich erinnere mich nicht mehr

--- app/src/main/java/com/example/isociable/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/isociable/ui/home/HomeFragment.java
@@ -1,35 +1,84 @@
 package com.example.isociable.ui.home;
 
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.LayerDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.constraintlayout.helper.widget.Layer;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.isociable.R;
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements View.OnClickListener {
 
-    private HomeViewModel homeViewModel;
-
+    HomeViewModel homeViewModel;
+    ImageView imageView;
+    LayerDrawable enviormentdrawable;
+    Drawable item;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
+
+
+
+
+
+        // Referenz auf LayerList
+        enviormentdrawable = (LayerDrawable) getResources().getDrawable(R.drawable.layerlistimage);
+
+        // Referenz auf ein Item in der Layerlist
+        item = enviormentdrawable.findDrawableByLayerId(R.id.gekauft1);
+
+        // Wenn Pflanze gekauft, wird Alpha gesetzt
+        homeViewModel.getgekauft1().observe(getViewLifecycleOwner(), new Observer<Boolean>() {
+            @Override
+            public void onChanged(Boolean aBoolean) {
+                if(homeViewModel.getgekauft1().getValue()==true){
+
+                    item.setAlpha(255);
+                }else{
+
+                    item.setAlpha(0);
+                }
+            }
+        });
+
+
+
+        Button button = (Button) root.findViewById(R.id.button);
+        button.setOnClickListener(this);
+        imageView = root.findViewById(R.id.imageView);
+        imageView.setImageDrawable(getResources().getDrawable(R.drawable.layerlistimage));
+/*        final TextView textView = root.findViewById(R.id.text_home);
         homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
             }
-        });
+        });*/
+
+
+
+
         return root;
+    }
+
+    @Override
+    public void onClick(View v) {
+        homeViewModel.setgekauft1();
+
+
     }
 }
--- app/src/main/java/com/example/isociable/ui/home/HomeViewModel.java
+++ app/src/main/java/com/example/isociable/ui/home/HomeViewModel.java
@@ -6,14 +6,39 @@ import androidx.lifecycle.ViewModel;
 
 public class HomeViewModel extends ViewModel {
 
-    private MutableLiveData<String> mText;
 
-    public HomeViewModel() {
-        mText = new MutableLiveData<>();
-    /*    mText.setValue("@string/hello_nature_lover");*/
+    public MutableLiveData<Boolean> gekauft1 = new MutableLiveData<Boolean>();
+
+    /*   private MutableLiveData<String> mText;
+    
+
+
+   *//* public HomeViewModel() {
+
+
+        *//**//*mText = new MutableLiveData<>();*//**//*
+
+  
+
+    *//**//*    mText.setValue("@string/hello_nature_lover");*//**//*
     }
+*//*
+
+
+
 
     public LiveData<String> getText() {
         return mText;
+    }*/
+
+    public HomeViewModel() {
+        gekauft1.setValue(false);
     }
+
+    public LiveData<Boolean> setgekauft1(){
+        gekauft1.setValue(true);
+        return gekauft1;
+    }
+    public LiveData<Boolean> getgekauft1(){ return gekauft1;}
+    
 }
